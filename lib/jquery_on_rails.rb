4f0d5af008cb0691fbb7908f4db4f09fb3862e62
Joe Khoobyar <joe@ankhcraft.com>
2010-05-23 14:05:33 -0400

    utilize ActiveSupport::Autoload

--- lib/jquery_on_rails.rb
+++ lib/jquery_on_rails.rb
@@ -1,4 +1,14 @@
+require 'active_support'
+
 module JQueryOnRails
   VERSION = File.read(File.expand_path('../../VERSION', __FILE__)).strip
+  
+  module Helpers
+    extend ActiveSupport::Autoload
+
+    autoload :JQueryHelper, 'jquery_on_rails/helpers/jquery_helper'
+    autoload :JQueryUiHelper, 'jquery_on_rails/helpers/jquery_ui_helper'
+  end
+
 	require 'jquery_on_rails/railtie' if defined? Rails
 end
